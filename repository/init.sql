33a528efc349981f843b53e53480f5120be0ae9f
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-02-14 21:39:07 +1100

    Remove more silly commas

--- repository/init.sql
+++ repository/init.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS race_summaries
 	venue_country TEXT,
 	venue_id TEXT,
 	venue_name TEXT,
-	venue_state TEXT,
+	venue_state TEXT
 );
 	/* title TEXT, */
 	/* pub_date DATE, */
@@ -26,31 +26,31 @@ CREATE TABLE IF NOT EXISTS race_forms
 (
 	id SERIAL PRIMARY KEY,
 	distance int
-	distance_type_id, TEXT,
+	distance_type_id TEXT,
 	generated TEXT,
 	race_comment TEXT,
 	race_comment_alternative TEXT,
 	silk_base_url TEXT,
 	track_condition TEXT,
-	weather_id TEXT,
+	weather_id TEXT
 );
 CREATE TABLE IF NOT EXISTS distance_types
 (
 	id TEXT PRIMARY KEY,
 	name TEXT,
-	short_name TEXT,
+	short_name TEXT
 );
 CREATE TABLE IF NOT EXISTS track_conditions
 (
 	id TEXT PRIMARY KEY,
 	name TEXT,
-	short_name TEXT,
+	short_name TEXT
 );
 CREATE TABLE IF NOT EXISTS weather_conditions
 (
 	id TEXT PRIMARY KEY,
 	icon_uri TEXT,
 	name TEXT,
-	short_name TEXT,
+	short_name TEXT
 );
 ALTER USER ladbrokes WITH PASSWORD 'hu8jmn3';
