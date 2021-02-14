924f4bc54a91450d4394f3e2ff4077c9c44c7be9
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-02-14 19:28:30 +1100

    Add basic types to db init file

--- repository/init.sql
+++ repository/init.sql
@@ -4,18 +4,19 @@ GRANT ALL PRIVILEGES ON DATABASE race_data_db TO ladbrokes;
 \connect race_data_db ladbrokes
 CREATE TABLE IF NOT EXISTS race_summaries
 (
-	id SERIAL PRIMARY KEY,
-	category_id UUID,
-	meeting_id,
-	meeting_name
-	race_form
-	race_id
-	race_name
-	race_number
-	venue_country
-	venue_id
-	venue_name
-	venue_state
+	id TEXT PRIMARY KEY,
+	advertised_start int,
+	category_id TEXT,
+	meeting_id TEXT,
+	meeting_name TEXT,
+	race_form int,
+	race_id TEXT,
+	race_name TEXT,
+	race_number int,
+	venue_country TEXT,
+	venue_id, TEXT,
+	venue_name TEXT,
+	venue_state TEXT,
 );
 	/* title TEXT, */
 	/* pub_date DATE, */
@@ -24,32 +25,32 @@ CREATE TABLE IF NOT EXISTS race_summaries
 CREATE TABLE IF NOT EXISTS race_forms
 (
 	id SERIAL PRIMARY KEY,
-	distance
-	distance_type
-	generated
-	race_comment TEXT
-	race_comment_alternative
-	silk_base_url
-	track_condition
-	weather_id
+	distance int
+	distance_type_id, TEXT,
+	generated TEXT,
+	race_comment TEXT,
+	race_comment_alternative TEXT,
+	silk_base_url TEXT,
+	track_condition TEXT,
+	weather_id TEXT,
 );
 CREATE TABLE IF NOT EXISTS distance_types
 (
-	id SERIAL PRIMARY KEY,
-	name
-	short_name
+	id TEXT PRIMARY KEY,
+	name TEXT,
+	short_name TEXT,
 );
 CREATE TABLE IF NOT EXISTS track_conditions
 (
-	id SERIAL PRIMARY KEY,
-	name
-	short_name
+	id TEXT PRIMARY KEY,
+	name TEXT,
+	short_name TEXT,
 );
 CREATE TABLE IF NOT EXISTS weather_conditions
 (
-	id SERIAL PRIMARY KEY,
-	icon_uri
-	name
-	short_name
+	id TEXT PRIMARY KEY,
+	icon_uri TEXT,
+	name TEXT,
+	short_name TEXT,
 );
 ALTER USER ladbrokes WITH PASSWORD 'hu8jmn3';
