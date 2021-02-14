0c3bd69815c4c1dbd4e02bc585a91bee9c2f3e1c
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-02-14 16:28:30 +1100

    Add some logging for debugging

--- racingservice/api/grpc/client/v1/racingclient.go
+++ racingservice/api/grpc/client/v1/racingclient.go
@@ -45,11 +45,14 @@ func (rc *RacingClient) GetRaces(req *proto.RacesRequest) (*proto.Races, error) 
 	defer conn.Close()
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+	log.Printf("RacingClient GetRaces received req: %v", req)
 
 	races, err := c.GetRaces(ctx, req)
 	if err != nil {
+		log.Printf("RacingClient GetRaces returning error: %v", err)
 		races = &proto.Races{}
 	}
+	log.Printf("RacingClient GetRaces returning error: %v", err)
 	return races, err
 
 }
