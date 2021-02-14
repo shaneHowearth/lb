71692f60548ae9341a941d7eaae4405965752378
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-02-14 18:47:05 +1100

    Add dockerfile

--- racingservice/api/grpc/client/v1/racingclient.go
+++ racingservice/api/grpc/client/v1/racingclient.go
@@ -47,7 +47,7 @@ func (rc *RacingClient) GetRaces(req *proto.RacesRequest) (*proto.Races, error) 
 	defer conn.Close()
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	log.Printf("RacingClient GetRaces received req: %v", req)
+	// log.Printf("RacingClient GetRaces received req: %v", req)
 
 	races, err := c.GetRaces(ctx, req)
 	if err != nil {
---
+++ racingservice/go.mod
@@ -0,0 +1,11 @@
+module github.com/shanehowearth/lb/racingservice
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/google/uuid v1.1.1
+	github.com/jackc/pgx/v4 v4.10.1
+	github.com/lib/pq v1.9.0 // indirect
+	google.golang.org/grpc v1.25.0
+)
